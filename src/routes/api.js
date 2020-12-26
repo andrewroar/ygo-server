875bfbbe041797ee880cfa290afeedc9c480699e
andrewroar <59775822+andrewroar@users.noreply.github.com>
2020-12-26 00:06:43 +0000

    28122020 comment out

--- src/routes/api.js
+++ src/routes/api.js
@@ -10,87 +10,87 @@ const bcrypt = require("bcrypt");
 
 //API
 
-const transformGoogleBooks = (googleBooks = []) => {
-  return googleBooks.map((googleBook) => {
-    return {
-      title: googleBook.volumeInfo.title,
-      subTitle: googleBook.volumeInfo.subTitle,
-      description: googleBook.volumeInfo.description,
-      authors: googleBook.volumeInfo.authors,
-      image: googleBook.volumeInfo.imageLinks.thumbnail,
-      link: googleBook.volumeInfo.previewLink,
-    };
-  });
-};
-
-const getBooksFromGoogle = async (req, res) => {
-  try {
-    const { searchTerm } = req.body;
-
-    const { data } = await axios.get(GOOGLE_URL, { params: { q: searchTerm } });
-
-    const books = transformGoogleBooks(data.items);
-
-    res.json({
-      results: books,
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-
-const saveBookInDb = async (req, res) => {
-  try {
-    const payload = req.body;
-
-    await db.Book.create(payload);
-
-    res.json({
-      success: true,
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-
-const removeBookInDb = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    await db.Book.findByIdAndDelete(id);
-
-    res.json({
-      success: true,
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-
-const getAllSavedBooks = async (_, res) => {
-  try {
-    const books = await db.Book.find({});
-
-    res.json({
-      results: books,
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-
-apiRouter.post("/books", getBooksFromGoogle);
-apiRouter.get("/save", getAllSavedBooks);
-apiRouter.post("/save", saveBookInDb);
-apiRouter.delete("/books/:id", removeBookInDb);
+//const transformGoogleBooks = (googleBooks = []) => {
+//  return googleBooks.map((googleBook) => {
+//    return {
+//      title: googleBook.volumeInfo.title,
+//      subTitle: googleBook.volumeInfo.subTitle,
+//      description: googleBook.volumeInfo.description,
+//      authors: googleBook.volumeInfo.authors,
+//      image: googleBook.volumeInfo.imageLinks.thumbnail,
+//      link: googleBook.volumeInfo.previewLink,
+//    };
+//  });
+//};
+//
+//const getBooksFromGoogle = async (req, res) => {
+//  try {
+//    const { searchTerm } = req.body;
+//
+//    const { data } = await axios.get(GOOGLE_URL, { params: { q: searchTerm } });
+//
+//    const books = transformGoogleBooks(data.items);
+//
+//    res.json({
+//      results: books,
+//    });
+//  } catch (error) {
+//    res.status(500).json({
+//      error: error.message,
+//    });
+//  }
+//};
+//
+//const saveBookInDb = async (req, res) => {
+//  try {
+//    const payload = req.body;
+//
+//    await db.Book.create(payload);
+//
+//    res.json({
+//      success: true,
+//    });
+//  } catch (error) {
+//    res.status(500).json({
+//      error: error.message,
+//    });
+//  }
+//};
+//
+//const removeBookInDb = async (req, res) => {
+//  try {
+//    const { id } = req.params;
+//
+//    await db.Book.findByIdAndDelete(id);
+//
+//    res.json({
+//      success: true,
+//    });
+//  } catch (error) {
+//    res.status(500).json({
+//      error: error.message,
+//    });
+//  }
+//};
+//
+//const getAllSavedBooks = async (_, res) => {
+//  try {
+//    const books = await db.Book.find({});
+//
+//    res.json({
+//      results: books,
+//    });
+//  } catch (error) {
+//    res.status(500).json({
+//      error: error.message,
+//    });
+//  }
+//};
+//
+//apiRouter.post("/books", getBooksFromGoogle);
+//apiRouter.get("/save", getAllSavedBooks);
+//apiRouter.post("/save", saveBookInDb);
+//apiRouter.delete("/books/:id", removeBookInDb);
 
 //////////////////////////////
 
@@ -151,12 +151,13 @@ const VoteCard = async (req, res) => {
   try {
     const { id } = req.params;
     const { vote } = req.body;
+    console.log(id);
 
     await db.CustomCards.findOneAndUpdate({ _id: id }, { vote: vote });
-
-    res.json({
-      success: true,
-    });
+    console.log(success),
+      res.json({
+        success: true,
+      });
   } catch (error) {
     res.status(200);
   }
